1bc9822a0ec910db814cabc5a90e9780df9ef5fb
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2024-10-31 11:52:15 +0100

    ♻️ Update : apply tailwind css classes for input component

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react';
-import Input from './Input';
-import Button from './Button';
+import { useState } from "react";
+import Input from "./Input";
+import Button from "./Button";
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -19,34 +19,55 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800"
+    >
+      <div className="flex flex-col gap-3 mb-6">
+      <Input
+          label="Email"
+          $invalid={emailNotValid}
+          type="email"
+          // className={emailNotValid ? 'invalid' : undefined}
+          onChange={(event) => handleInputChange("email", event.target.value)}
+        />
+        <Input
+          label="Password"
+          type="password"
+          $invalid={passwordNotValid}
+          // className={passwordNotValid ? 'invalid' : undefined}
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+        />
+      </div>
 
-      <Input
-        label="Email"
-        $invalid={emailNotValid}
-        type="email"
-        // className={emailNotValid ? 'invalid' : undefined}
-        onChange={(event) => handleInputChange('email', event.target.value)}
-      />
-      <Input
-        label="Password"
-        type="password"
-        $invalid={passwordNotValid}
-        // className={passwordNotValid ? 'invalid' : undefined}
-        onChange={(event) =>
-          handleInputChange('password', event.target.value)
-        }
-      />
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
-        <Button className='button' onClick={handleLogin}>Sign In</Button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
 }
+        //  <Input
+        //   label="Email"
+        //   $invalid={emailNotValid}
+        //   type="email"
+        //   // className={emailNotValid ? 'invalid' : undefined}
+        //   onChange={(event) => handleInputChange("email", event.target.value)}
+        // />
+        // <Input
+        //   label="Password"
+        //   type="password"
+        //   $invalid={passwordNotValid}
+        //   // className={passwordNotValid ? 'invalid' : undefined}
+        //   onChange={(event) =>
+        //     handleInputChange("password", event.target.value)
+        //   }
+        // />
